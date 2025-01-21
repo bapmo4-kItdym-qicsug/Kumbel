56ef87dc8fe88efd7233fc9aae3425c61bdde16a
Veronika <168175462+bapmo4-kItdym-qicsug@users.noreply.github.com>
2025-01-21 16:35:16 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-function convertC (degreeCelsius){
-    let degreeFarengeity = ((degreeCelsius  *9 / 5) + 32);
-    console.log(degreeFarengeity + "°F");
+const glodalVar = "I'm global varible";
+function scope(){
+    const localVar = "I'm local varible";
+    const blockVar = "I'm block varible";
+    console.log(glodalVar, "global var inside");
+    console.log(localVar, "local var inside);");
+    console.log(blockVar, "block var inside"); // Блочная область видимости доступна только внутри какого-либо блока коды, в котором она определена.
+    // Локальная работает только внутри функции.
 }
-    convertC (25);
-    convertC (95);
 
-function convertF (degreeFarengeity){
-    degreeCel = ((degreeFarengeity - 32) *5 / 9);
-    console.log(degreeCel + "°C");
-}
-    convertF(77);
-    convertF(203);
+scope();
+console.log(glodalVar, "global var outside");
+console.log(localVar, "local var outside);");
+console.log(blockVar, "block var outside");
+
+// Тут я пыталась создать две функции, с одинаковымми переменными что бы прговерить что они мне будут друг с другом кофликтовать 
+// const appropriate = function(){
+//     const blockVar = "I'm block varible";
+//     console.log(blockVar, "block var inside");
+// }
+// appropriate();
+// console.log(blockVar, "block var outside1");
+
+// function create(){
+//     const blockVar = "I'm block varible";
+//     console.log(blockVar, "block var inside");
+// }
+// create();
+// console.log(blockVar, "block var outside2");
